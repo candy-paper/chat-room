824cbdb05245ef195e594ece11d82516fd64969a
lixiaolin <lixiaolin@hichao.com>
2016-11-16 09:32:13 +0800

    add chat room

---
+++ login.js
@@ -0,0 +1,29 @@
+/**
+ * Created by lilin on 16/7/21.
+ */
+
+$(document).ready(function (wrapper) {
+    (function (doc, win) {
+        var docEl = doc.getElementsByClassName(wrapper)[0]||doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+                var clientWidth = docEl.clientWidth;
+                if(doc.getElementsByClassName(wrapper)){
+                 /*   clientWidth = docEl.style.width.slice(0,-2);*/
+                    clientWidth = 750;
+
+                }
+
+                if (!clientWidth) return;
+                docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
+            };
+        recalc();
+        if (!doc.addEventListener) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        doc.addEventListener('DOMContentLoaded', recalc, false);
+    })(document, window);
+});
+alert(123);
+
+
+
